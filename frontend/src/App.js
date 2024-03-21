bb6628e75039ba70d8204eb61cfa2ad387e23c7a
namra22 <namraikram78@gmail.com>
2024-03-22 02:20:53 +0500

    Initial project setup: Installed server and client dependencies

---
+++ frontend/src/App.js
@@ -0,0 +1,10 @@
+function App() {
+    return (
+      <div className="min-h-screen p-6 bg-white text-gray-600 text-lg">
+       <h1>appppppppppp</h1>
+      </div>
+    );
+  }
+  
+  export default App;
+  
